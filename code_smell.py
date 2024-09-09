3a6bc68841214631abb990a1c531e8fc9feee46a
rohrohaaa <rosanesilvasilvafreitas@gmail.com>
2024-09-09 20:03:37 -0300

    add smelly code from ArjanCodes

--- code_smell.py
+++ code_smell.py
@@ -1,125 +1,122 @@
-import sys
-import math
+"""
+Very advanced Employee management system.
+"""
 
-def calculo(numero1, numero2, numero3, numero4, numero5):
-   a = numero1 + numero2 + numero3 + numero4 + numero5
-   print (a)
-   return numero1 + numero2 + numero3 + numero4 + numero5
+from dataclasses import dataclass
+from typing import List
 
-def funcao_nova(valor, a, b):
-    if valor > 1:
-       if valor < 2:
-           passou = "Sim"
-       else:
-           passou = "Não"
-    if valor > 2:
-       if valor < 3:
-           passou = "Sim"
-       else:
-           passou = "Não"
-    if valor > 3:
-       if valor < 4:
-           passou = "Sim"
-       else:
-           passou = "Não"
-    if valor > 4:
-       if valor < 5:
-           passou = "Sim"
-       else:
-           passou = "Não"
-    if valor > 5 :
-       passou = "invalido"
-    if valor < 1 :
-       passou = "invalido"
-
-    for i in lista:
-        for j in lista:
-            for k in lista:
-                print(i, j, k)
-
-    c = a + b
-    print("Resultado: " + c)
-    d = a * b
-    print("Produto: " + d)
-    if a > b:
-       print("a é maior que b")
-    if a < b:
-       print("b é maior que a")
-    if a == b:
-       print("a e igual a b")
-
-   # if a == b :
-   #     a = a - 1
-   #     print("Valor reduzido de a: " + a)
-
-    media = (a + b) / 2
-    soma = a + b
-    if a > b or a == b:
-        subtracao = a - b
-    if b > a:
-        subtracao = b - a
-    if not b == 0 :
-        divisao = a/b
-    if b == 0 :
-        divisao = "erro"
-    multiplicacao = a * b
-
-    if valor == 46.5482:
-        valor = valor * 5
-
-    return media, soma, subtracao, divisao, multiplicacao, passou
-    return "Sim"
-
-def funcao(lista, a, b):
-    pass
-
-def outra_funcao(a,b):
-   c = a + b
-   print("Resultado: " + c)
-   d = a * b
-   print("Produto: " + d)
-   if a > b:
-       print("a é maior que b")
-   if a < b:
-       print("b é maior que a")
-   if a == b:
-       print("a e igual a b")
+FIXED_VACATION_DAYS_PAYOUT = 5  # The fixed nr of vacation days that can be paid out.
 
 
-class Numero:
-   def __init__(self,valor):
-       self.valor = valor
+@dataclass
+class Employee:
+    """Basic representation of an employee at the company."""
+
+    name: str
+    role: str
+    vacation_days: int = 25
+
+    def take_a_holiday(self, payout: bool) -> None:
+        """Let the employee take a single holiday, or pay out 5 holidays."""
+        if payout:
+            # check that there are enough vacation days left for a payout
+            if self.vacation_days < FIXED_VACATION_DAYS_PAYOUT:
+                raise ValueError(
+                    f"You don't have enough holidays left over for a payout.\
+                        Remaining holidays: {self.vacation_days}."
+                )
+            try:
+                self.vacation_days -= FIXED_VACATION_DAYS_PAYOUT
+                print(f"Paying out a holiday. Holidays left: {self.vacation_days}")
+            except Exception:
+                # this should never happen
+                pass
+        else:
+            if self.vacation_days < 1:
+                raise ValueError(
+                    "You don't have any holidays left. Now back to work, you!"
+                )
+            self.vacation_days -= 1
+            print("Have fun on your holiday. Don't forget to check your emails!")
 
 
-   def soma(self, numero):
-       self.valor = self.valor + numero
-  
-   def sub(self, numero):
-       self.valor = self.valor - numero
-  
-   def mult(self, numero):
-       self.valor = self.valor * numero
-  
-   def div(self, numero):
-       if not numero == 0:
-           self.valor = self.valor / numero
-       else:
-           print("Erro")
-  
-   def valor(self):
-       print(self.valor)
-  
-   def valor_pi(self):
-       return math.pi
-  
-   def area_quad(self,lado1, lado2):
-       area = lado1*lado2
-       print(area)
-       return(area)
+@dataclass
+class HourlyEmployee(Employee):
+    """Employee that's paid based on number of worked hours."""
 
-   def printa(self, string):
-       print(string)
+    hourly_rate: float = 50
+    amount: int = 10
 
-   def printa_alfabeto(self):
-       for letra in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
-           print (letra)
+
+@dataclass
+class SalariedEmployee(Employee):
+    """Employee that's paid based on a fixed monthly salary."""
+
+    monthly_salary: float = 5000
+
+
+class Company:
+    """Represents a company with employees."""
+
+    def __init__(self) -> None:
+        self.employees: List[Employee] = []
+
+    def add_employee(self, employee: Employee) -> None:
+        """Add an employee to the list of employees."""
+        self.employees.append(employee)
+
+    def find_managers(self) -> List[Employee]:
+        """Find all manager employees."""
+        managers = []
+        for employee in self.employees:
+            if employee.role == "manager":
+                managers.append(employee)
+        return managers
+
+    def find_vice_presidents(self) -> List[Employee]:
+        """Find all vice-president employees."""
+        vice_presidents = []
+        for employee in self.employees:
+            if employee.role == "vice_president":
+                vice_presidents.append(employee)
+        return vice_presidents
+
+    def find_interns(self) -> List[Employee]:
+        """Find all interns."""
+        interns = []
+        for employee in self.employees:
+            if employee.role == "intern":
+                interns.append(employee)
+        return interns
+
+    def pay_employee(self, employee: Employee) -> None:
+        """Pay an employee."""
+        if isinstance(employee, SalariedEmployee):
+            print(
+                f"Paying employee {employee.name} a monthly salary of ${employee.monthly_salary}."
+            )
+        elif isinstance(employee, HourlyEmployee):
+            print(
+                f"Paying employee {employee.name} a hourly rate of \
+                ${employee.hourly_rate} for {employee.amount} hours."
+            )
+
+
+def main() -> None:
+    """Main function."""
+
+    company = Company()
+
+    company.add_employee(SalariedEmployee(name="Louis", role="manager"))
+    company.add_employee(HourlyEmployee(name="Brenda", role="president"))
+    company.add_employee(HourlyEmployee(name="Tim", role="intern"))
+
+    print(company.find_vice_presidents())
+    print(company.find_managers())
+    print(company.find_interns())
+    company.pay_employee(company.employees[0])
+    company.employees[0].take_a_holiday(False)
+
+
+if __name__ == "__main__":
+    main()
